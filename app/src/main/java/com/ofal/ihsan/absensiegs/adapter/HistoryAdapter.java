fb6195279a09b38c44a57ffea2ddf2c564015bb3
ihsan540 <ihsanab31@gmail.com>
2018-07-18 08:31:15 +0700

    Absen EGS update 1

---
+++ app/src/main/java/com/ofal/ihsan/absensiegs/adapter/HistoryAdapter.java
@@ -0,0 +1,13 @@
+package com.ofal.ihsan.absensiegs.adapter;
+
+/**
+ * Created by
+ * Name     : Ihsan Abdurahman
+ * Email    : ihsanab31@gmail.com
+ * WA       : 0878253827096
+ * on 17/07/2018
+ * ------------------------------
+ * This class for
+ */
+public class HistoryAdapter {
+}
---
+++ app/src/main/java/com/ofal/ihsan/absensiegs/api/EndPoint.java
@@ -0,0 +1,13 @@
+package com.ofal.ihsan.absensiegs.api;
+
+/**
+ * Created by
+ * Name     : Ihsan Abdurahman
+ * Email    : ihsanab31@gmail.com
+ * WA       : 0878253827096
+ * on 17/07/2018
+ * ------------------------------
+ * This class for
+ */
+public interface EndPoint {
+}
