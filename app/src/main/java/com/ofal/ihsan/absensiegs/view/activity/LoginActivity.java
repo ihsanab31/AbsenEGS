887a42527e8c78382953447854674a8353b46ee8
ihsan540 <ihsanab31@gmail.com>
2018-07-18 14:24:12 +0700

    Updaate onclick

--- app/src/main/java/com/ofal/ihsan/absensiegs/view/activity/LoginActivity.java
+++ app/src/main/java/com/ofal/ihsan/absensiegs/view/activity/LoginActivity.java
@@ -1,11 +1,22 @@
 package com.ofal.ihsan.absensiegs.view.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
 import com.ofal.ihsan.absensiegs.R;
-import com.ofal.ihsan.absensiegs.view.base.NoActionBarConfig;
 import com.ofal.ihsan.absensiegs.view.base.NormalActivity;
 
+import butterknife.BindView;
+import butterknife.OnClick;
+
 public class LoginActivity extends NormalActivity {
+
+    @BindView(R.id.et_username)
+    EditText etUsername;
+    @BindView(R.id.et_password)
+    EditText etPassword;
 
     @Override
     protected int getActivityView() {
@@ -21,5 +32,20 @@ public class LoginActivity extends NormalActivity {
     @Override
     protected boolean isActionBarEnable() {
         return false;
+    }
+
+    @OnClick({R.id.btn_login, R.id.btn_createpass})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.btn_login:
+                login();
+                break;
+            case R.id.btn_createpass:
+                startActivity(new Intent(getApplicationContext(), CreatePasswordActivity.class));
+                break;
+        }
+    }
+
+    private void login() {
     }
 }
