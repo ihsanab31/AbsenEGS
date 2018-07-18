38f000341d2ed7549e9aa8d02459b63ef9df9f9c
ihsan540 <ihsanab31@gmail.com>
2018-07-18 11:56:38 +0700

    Updaate asset

--- app/src/main/java/com/ofal/ihsan/absensiegs/view/activity/LoginActivity.java
+++ app/src/main/java/com/ofal/ihsan/absensiegs/view/activity/LoginActivity.java
@@ -1,15 +1,26 @@
 package com.ofal.ihsan.absensiegs.view.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
 import com.ofal.ihsan.absensiegs.R;
+import com.ofal.ihsan.absensiegs.view.base.NoActionBarConfig;
+import com.ofal.ihsan.absensiegs.view.base.NormalActivity;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends NormalActivity {
+
+    @Override
+    protected int getActivityView() {
+        return R.layout.activity_login;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        NoActionBarConfig noActionBarConfig = new NoActionBarConfig();
+        noActionBarConfig.fullScreen(this);
+    }
+
+    @Override
+    protected boolean isActionBarEnable() {
+        return false;
     }
 }
